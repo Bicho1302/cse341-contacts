565a313c944be15c877062b137bc4c05d873ddcb
Bicho1302 <victorsn13022003@gmail.com>
2026-07-18 12:03:34 -0600

    Fix Swagger URL for Render

--- swagger.js
+++ swagger.js
@@ -1,10 +1,16 @@
 const swaggerAutogen = require("swagger-autogen")();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const doc = {
   info: {
     title: "Contacts API",
     description: "API documentation for managing contacts"
-  }
+  },
+  host: isProduction
+    ? "cse341-contacts-4gwr.onrender.com"
+    : "localhost:3000",
+  schemes: isProduction ? ["https"] : ["http"]
 };
 
 const outputFile = "./swagger-output.json";
